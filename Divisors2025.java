820144afaee07adab6797a1c40745131f08b306b
strawbwiees <noellamarizbeltran01@gmail.com>
2025-09-30 13:21:15 +0800

    Add files via upload

---
+++ Divisors2025.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Divisors2025 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Ask the user to enter a number
+        System.out.print("Enter a number: ");
+        int N = sc.nextInt();
+
+        // Loop through all numbers from 1 to N
+        System.out.print("Common divisors with 2025: ");
+        for (int i = 1; i <= N; i++) {
+                       if (N % i == 0 && 2025 % i == 0) {  // Check if i is a divisor of both N and 2025
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+}
